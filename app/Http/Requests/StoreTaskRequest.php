407bd5f97127daee129fae908d342c2e705cc0c2
dinar.daianov <dinardaian@gmail.com>
2025-02-22 23:29:15 +0300

    first commit

---
+++ app/Http/Requests/StoreTaskRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreTaskRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'required',
+            'description' => 'required',
+            'due_date' => 'required',
+            'priority' => 'required',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'title.required' => 'Title is required.',
+            'description.required' => 'Description is required.',
+            'due_date.required' => 'Due date is required.',
+            'priority.required' => 'Priority is required.',
+        ];
+    }
+}
